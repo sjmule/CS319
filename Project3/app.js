2c4abcc37677058c7eb765e0af3f9e4bcdb76a98
Samuel Eue <fyreye@gmail.com>
2016-04-29 00:02:08 -0500

    Project 3 Jeopardy
    put together rough board and question selection system

---
+++ Project3/app.js
@@ -0,0 +1,121 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.service('questionCollection', function() {
+	var categories =
+		[ {name: "Therapists",
+			Questions: [ {value: 200, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 400, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 600, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 800, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 1000, status: true, question: "1984", answer: "George Orwell", DD: false}]
+		  },
+		  {name: "Catch These Men",
+			Questions: [ {value: 200, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 400, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 600, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 800, status: true, question: "1984", answer: "George Orwell", DD: true},
+					 {value: 1000, status: true, question: "1984", answer: "George Orwell", DD: false}]
+		  },
+		  {name: "Famous Horsemen",
+			Questions: [ {value: 200, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 400, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 600, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 800, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 1000, status: true, question: "1984", answer: "George Orwell", DD: false}]
+		  },
+			{name: "The Pen Is Mightier",
+			Questions: [ {value: 200, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 400, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 600, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 800, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 1000, status: true, question: "1984", answer: "George Orwell", DD: false}]
+		  },
+		  {name: "An Album Cover",
+			Questions: [ {value: 200, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 400, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 600, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 800, status: true, question: "1984", answer: "George Orwell", DD: false},
+					 {value: 1000, status: true, question: "1984", answer: "George Orwell", DD: false}]
+		  }]
+	this.getCategories = function() {
+		return categories;
+	}
+});
+
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/cell/:value/:category", {
+        controller: "cell",
+        templateUrl: "Cell.html"
+    });
+});
+
+
+
+app.controller('myController', function ($scope, questionCollection) {
+	$scope.categories = questionCollection.getCategories();
+});
+
+
+app.controller('cell', function ($scope, $routeParams, questionCollection) {
+	$scope.category = $routeParams.category;
+	$scope.value = $routeParams.value;
+	var cataQuestions = angular.fromJson(questionCollection.getCategories());
+	var found = false;
+	for ( i = 0; i < cataQuestions.length; i++)
+	{
+		if (cataQuestions[i].name == $routeParams.category )
+		{
+			for ( j = 0; j < cataQuestions[i].Questions.length; j++)
+			{
+				if (cataQuestions[i].Questions[j].value == $routeParams.value)
+				{
+					$scope.answer = cataQuestions[i].Questions[j].answer;
+					$scope.question = cataQuestions[i].Questions[j].question;
+					$scope.DD = cataQuestions[i].Questions[j].DD;
+					found = true;
+					break;
+				}else
+				$scope.answer = "Val not found";
+			}
+			if ( found == true )
+				break;
+			
+		}else
+			$scope.answer = "Category not found";
+	}
+	
+		
+	
+});
+
+
+
+
+/*
+app.controller('book', function ($scope, $routeParams, bookCollection) {
+	$scope.shelf = $routeParams.shelf;
+	$scope.nam = $routeParams.bookName;
+	var bookShelves = angular.fromJson(bookCollection.getBookShelves());
+	var found = false;
+	for ( i = 0; i < bookShelves.length; i++)
+	{
+		if (bookShelves[i].name == $routeParams.shelf)
+		{
+			for ( j = 0; j < bookShelves[i].books.length; j++)
+			{
+				if (bookShelves[i].books[j].name == $routeParams.bookName)
+				{
+					$scope.title = bookShelves[i].books[j].title;
+					$scope.author = bookShelves[i].books[j].author;
+					$scope.status = bookShelves[i].books[j].status;
+					found = true;
+					break;
+				}
+			}
+			if ( found == true )
+				break;
+		}
+	}
+});
+*/
